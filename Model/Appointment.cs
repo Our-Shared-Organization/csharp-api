40dbca2a9ed85df9d65c724584fae3befe95fd86
Мухамадиев Эмиль <Nizametdinow.ru@gmail.com>
2025-09-16 13:19:36 +0500

    Добавление регистрации и авторизации на стороне API

---
+++ Model/Appointment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace whatever_api.Model;
+
+public partial class Appointment
+{
+    public int AppointmentId { get; set; }
+
+    public int ClientId { get; set; }
+
+    public int ServiceId { get; set; }
+
+    public DateTime AppointmentDate { get; set; }
+
+    public DateTime EndTime { get; set; }
+
+    public string? Status { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public virtual User Client { get; set; } = null!;
+
+    public virtual Service Service { get; set; } = null!;
+}
